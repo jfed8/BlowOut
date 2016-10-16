51d2634eb058a78a58937bddd66d1d965466c63d
jfed8 <jadenfeddock@gmail.com>
2016-10-16 00:07:27 -0600

    Added Contact Page

---
+++ BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public string Index()
+        {
+            return "Please call Support at <strong><u>801-555-1212</u></strong>. Thank you!";
+        }
+
+        public string Email(string name, string email)
+        {
+            return "Thank you " + name + ". An email will be sent to " + email;
+        }
+
+    }
+}
