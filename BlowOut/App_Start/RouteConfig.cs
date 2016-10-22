85f0b38373082d4ef95373cf614dfc38ed71c089
jfed8 <jadenfeddock@gmail.com>
2016-10-22 14:27:08 -0600

    RouteConfig

--- BlowOut/App_Start/RouteConfig.cs
+++ BlowOut/App_Start/RouteConfig.cs
@@ -14,16 +14,18 @@ namespace BlowOut
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Email",
+                url: "{controller}/{action}/{name}/{email}",
+                defaults: new { controller = "Index", name = "Test", email = "test@test.com" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
 
-            routes.MapRoute(
-                name: "Email",
-                url: "{controller}/{action}/{name}/{email}",
-                defaults: new { controller = "Index", name = "Test", email = "test@test.com" }
-            );
+            
         }
     }
 }
